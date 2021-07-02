64d2a0925ecbebabd0749819ca01c3a6a70b98c8
Antony Ruby Alexia <alexia@m2p.in>
2021-07-02 14:15:38 +0530

    Added bloc

--- lib/bloc.dart
+++ lib/bloc.dart
@@ -1,21 +1,20 @@
-// import 'package:bloc/bloc.dart';
+import 'package:bloc/bloc.dart';
 
-// // ignore: public_member_api_docs
-// class EchoBlocDelegate extends BlocObserver {
-//   @override
-//   // ignore: unnecessary_overrides
-//   void onEvent(Bloc bloc, Object event) {
-//     super.onEvent(bloc, event);
-//   }
+class EchoBlocDelegate extends BlocObserver {
+  @override
+  // ignore: unnecessary_overrides, always_specify_types
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+  }
 
-//   @override
-//   void onTransition(Bloc bloc, Transition transition) {
-//     super.onTransition(bloc, transition);
-
-//     @override
-//     // ignore: unused_element
-//     void onError(Cubit bloc, Object error, StackTrace stacktrace) {
-//       super.onError(bloc, error, stacktrace);
-//     }
-//   }
-// }
+  @override
+  // ignore: always_specify_types
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    @override
+    // ignore: unused_element, always_specify_types
+    void onError(Cubit bloc, Object error, StackTrace stacktrace) {
+      super.onError(bloc, error, stacktrace);
+    }
+  }
+}
